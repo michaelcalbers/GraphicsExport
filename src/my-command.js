e63e30934c75416690c47cfa4dfe3fcd099dda9f
Michael C. Albers <michael.albers@autodesk.com>
2018-01-16 08:50:07 -0800

    showMessage testing

--- src/my-command.js
+++ src/my-command.js
@@ -1,4 +1,17 @@
 export default function(context) {
-	context.document.showMessage("Export Something!!!!!!")
-	var options = { "scales" : "1, 2, 3", "formats" : "png, svg, jpg" }
+	//context.document.showMessage("Export Something!!!!!!")
+	//var options = { "scales" : "1, 2, 3", "formats" : "png, svg, jpg" }
+	//context.selectedDocument.selectedLayers.iterate(function(layer) {
+	//	layer.export(options)
+    //})
+
+
+	const selectedLayers = context.selection
+	const selectedCount = selectedLayers.length
+
+	if (selectedCount === 0) {
+		context.document.showMessage('No layers are selected.')
+	} else {
+		context.document.showMessage(`${selectedCount} layers selected.`)
+	}
 }
