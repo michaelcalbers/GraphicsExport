4fc9767a800aa27285bd98d5188f68e905f6d42f
Michael C. Albers <michael.albers@autodesk.com>
2018-01-16 10:54:26 -0800

    cleaning
    cleaning up old experiments to try some new experiments

--- src/my-command.js
+++ src/my-command.js
@@ -1,10 +1,5 @@
 export default function(context) {
 	//context.document.showMessage("Export Something!!!!!!")
-	//var options = { "scales" : "1, 2, 3", "formats" : "png, svg, jpg" }
-	//context.selectedDocument.selectedLayers.iterate(function(layer) {
-	//	layer.export(options)
-    //})
-
 
 	const selectedLayers = context.selection
 	const selectedCount = selectedLayers.length
