f92576ad9afe89f27f03d4f886a7269a43b44f4c
Michael C. Albers <michael.albers@autodesk.com>
2018-01-17 13:56:52 -0800

    good spot to track progress

--- src/my-command.js
+++ src/my-command.js
@@ -1,17 +1,20 @@
+//var onRun = function(context) {
 export default function(context) {
-	//context.document.showMessage("Export Something!!!!!!")
+	log("Starting")
+	
+		const selectedLayers = context.selection
+		const selectedCount = selectedLayers.length
+		document = context.document
 
-	const selectedLayers = context.selection
-	const selectedCount = selectedLayers.length
-	document = context.document
+		if (selectedCount === 0) {
+			message = "     No layers selected."
+		} else if (selectedCount === 1) {
+			message = "     1 layer selected."
+		} else {
+			message = "     " + selectedCount + " layers selected."
+		}
+		document.showMessage(message); // on Screen
+		log(message); // to log/Console
 
-	if (selectedCount === 0) {
-		message = "No layers selected."
-	} else if  (selectedCount === 1) {
-		message = "1 layer selected."
-	} else {
-		message = selectedCount + " layers selected."
-	}
-	document.showMessage(message); // on Screen
-	log(message); // to log/Console
+	log("Ending")
 }
